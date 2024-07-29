400cfbb4d813909ba014359752e2a0cbf52d00de
Luis Pina <luispia1290@gmail.com>
2024-07-29 12:01:43 -0600

    Se agrego componente StepLine y se agreg condicion

--- src/templates/MainTemplate.tsx
+++ src/templates/MainTemplate.tsx
@@ -1,17 +1,21 @@
-import { Navigate, Route, Routes } from "react-router-dom"
-import { Bebidas, Desayunos, Postres, Promos, Waffles } from "../pages"
-import { Box, Container, createTheme, CssBaseline, ThemeProvider } from "@mui/material"
-import { Menu } from "../components"
-
+import { Navigate, Route, Routes, useLocation } from "react-router-dom";
+import { Bebidas, Desayunos, Postres, Promos, Waffles } from "../pages";
+import {
+  Box,
+  Container,
+  createTheme,
+  CssBaseline,
+  ThemeProvider,
+} from "@mui/material";
+import { Menu, StepLine } from "../components";
 
 const defaultTheme = createTheme();
 
 export const MainTemplate = () => {
-
-
+  const { pathname } = useLocation();
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Box sx={{ display: 'flex' }}>
+      <Box sx={{ display: "flex" }}>
         <CssBaseline />
         <Menu />
         <Container maxWidth="sm" sx={{ mt: 4, mb: 4 }}>
@@ -21,12 +25,22 @@ export const MainTemplate = () => {
             <Route path="/bebidas" element={<Bebidas />} />
             <Route path="/postres" element={<Postres />} />
             <Route path="/promos" element={<Promos />} />
-            <Route path="/" element={<Navigate to='/Home' />} />
+            <Route path="/" element={<Navigate to="/Home" />} />
           </Routes>
         </Container>
-
       </Box>
 
+      {pathname === "/waffles" ? (
+        <div
+          style={{
+            width: "100%",
+            display: "flex",
+            justifyContent: "left",
+          }}
+        >
+          <StepLine />
+        </div>
+      ) : null}
     </ThemeProvider>
-  )
-}
+  );
+};
