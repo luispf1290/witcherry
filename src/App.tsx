62828b9cbbc790e79f395f5c315066238a2fcfc7
Luis Pina <luispia1290@gmail.com>
2024-08-06 11:56:14 -0600

    Se modificaron rutas

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,14 @@
-
-import { Home } from './pages';
-import { MainTemplate } from './templates/MainTemplate'
-import { Route, Routes } from 'react-router-dom';
-
+import { Home } from "./pages";
+import { MainTemplate } from "./templates/MainTemplate";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-
-
   return (
-
     <Routes>
-      <Route 
-      path='/*' 
-        element={
-          <MainTemplate />
-        }
-      />
-      <Route 
-        path='/home'
-        element={
-          <Home/>
-        }
-      />
+      <Route path="/witcherry/*" element={<MainTemplate />} />
+      <Route path="/witcherry/home" element={<Home />} />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
