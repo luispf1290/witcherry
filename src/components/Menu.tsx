16f51f96c6b149fa8dd76f4ebe3b45c19aa38d9d
Luis Pina <luispia1290@gmail.com>
2024-08-06 11:56:35 -0600

    Se modificaron links de paginas

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,55 +1,60 @@
-
-import { useState } from 'react';
-import logo from '../assets/logo-witcherry.svg';
-import arrow from '../assets/chevron.svg'
-import bebidas from '../assets/menu/bebidas-witcherry.svg';
-import desayuno from '../assets/menu/desayuno-witcherry.svg';
-import postres from '../assets/menu/postres-witcherry.svg';
-import promos from '../assets/menu/promos-witcherry.svg';
-import waffles from '../assets/menu/waffles-witcherry.svg';
-import './menu.css'
-import { NavLink } from 'react-router-dom';
+import { useState } from "react";
+import logo from "../assets/logo-witcherry.svg";
+import arrow from "../assets/chevron.svg";
+import bebidas from "../assets/menu/bebidas-witcherry.svg";
+import desayuno from "../assets/menu/desayuno-witcherry.svg";
+import postres from "../assets/menu/postres-witcherry.svg";
+import promos from "../assets/menu/promos-witcherry.svg";
+import waffles from "../assets/menu/waffles-witcherry.svg";
+import "./menu.css";
+import { NavLink } from "react-router-dom";
 
 export const Menu = () => {
-
-  const [toggle, setToggle] = useState(false)
-
+  const [toggle, setToggle] = useState(false);
 
   const handleClickActive = () => {
-    setToggle(!toggle)
-  }
+    setToggle(!toggle);
+  };
   return (
     <>
-      <header className='header'>
-        <button className={`button-nav ${!toggle ? 'btn-active-border' : ''}`} onClick={handleClickActive}>
-          <div className='cont-buton'>
-            <img src={logo} className='logo' />
-            <img src={arrow} alt="arrow" style={{ marginTop: '10px' }} className={`arrow ${toggle ? 'activo' : ''}`} />
+      <header className="header">
+        <button
+          className={`button-nav ${!toggle ? "btn-active-border" : ""}`}
+          onClick={handleClickActive}
+        >
+          <div className="cont-buton">
+            <img src={logo} className="logo" />
+            <img
+              src={arrow}
+              alt="arrow"
+              style={{ marginTop: "10px" }}
+              className={`arrow ${toggle ? "activo" : ""}`}
+            />
           </div>
         </button>
-        <nav className={`nav ${toggle ? 'current' : ''}`} >
-          <ul className='list-items'>
-            <NavLink to={'/waffles'}>
+        <nav className={`nav ${toggle ? "current" : ""}`}>
+          <ul className="list-items">
+            <NavLink to={"/witcherry/waffles"}>
               <li className="items">
                 <img src={waffles} alt="waffles" />
               </li>
             </NavLink>
-            <NavLink to={'/desayunos'}>
+            <NavLink to={"/witcherry/desayunos"}>
               <li className="items">
                 <img src={desayuno} alt="desayuno" />
               </li>
             </NavLink>
-            <NavLink to={'/bebidas'}>
+            <NavLink to={"/witcherry/bebidas"}>
               <li className="items">
                 <img src={bebidas} alt="bebidas" />
               </li>
             </NavLink>
-            <NavLink to={'/postres'}>
+            <NavLink to={"/witcherry/postres"}>
               <li className="items">
                 <img src={postres} alt="postres" />
               </li>
             </NavLink>
-            <NavLink to={'/promos'}>
+            <NavLink to={"/witcherry/promos"}>
               <li className="items">
                 <img src={promos} alt="promos" />
               </li>
@@ -58,5 +63,5 @@ export const Menu = () => {
         </nav>
       </header>
     </>
-  )
-}
+  );
+};
