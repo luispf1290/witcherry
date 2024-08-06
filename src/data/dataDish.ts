7da996fea3aecce6d755d8107bc007d06a5d06f0
Luis Pina <luispia1290@gmail.com>
2024-08-06 10:23:46 -0600

    Se  creo archivo para agregar los paltillos

---
+++ src/data/dataDish.ts
@@ -0,0 +1,76 @@
+interface propsDish {
+    id: number,
+    name: string,
+    tipo:string,
+    img:string,
+    ingredientes: string[], 
+    recomendaciones:string[]
+}
+
+
+export const dishElements: propsDish[] = [
+    {
+        id:1,
+        name: 'Chocobanana',
+        tipo: 'dulce',
+        img:'',
+        ingredientes: ['Platano', 'Fresas', 'Moras Azules'],
+        recomendaciones: ['Cafe', 'Malteada', 'frape']
+    },
+    {
+        id:2,
+        name: 'Bosque Rojo',
+        tipo: 'dulce',
+        img:'',
+        ingredientes: ['Platano', 'Fresas', 'Moras Azules'],
+        recomendaciones: ['Cafe', 'Malteada', 'frape']
+    },
+    {
+        id:3,
+        name: 'Tentacion de Eva',
+        tipo: 'dulce',
+        img:'',
+        ingredientes: ['Platano', 'Fresas', 'Moras Azules'],
+        recomendaciones: ['Cafe', 'Malteada', 'frape']
+    },
+    {
+        id:4,
+        name: 'Brownie',
+        tipo: 'dulce',
+        img:'',
+        ingredientes: ['Platano', 'Fresas', 'Moras Azules'],
+        recomendaciones: ['Cafe', 'Malteada', 'frape']
+    },
+    {
+        id:5,
+        name: 'Caramelonions',
+        tipo: 'dulce',
+        img:'',
+        ingredientes: ['Platano', 'Fresas', 'Moras Azules'],
+        recomendaciones: ['Cafe', 'Malteada', 'frape']
+    },
+    {
+        id:6,
+        name: 'Sangre de Bufalo',
+        tipo: 'dulce',
+        img:'',
+        ingredientes: ['Platano', 'Fresas', 'Moras Azules'],
+        recomendaciones: ['Cafe', 'Malteada', 'frape']
+    },
+    {
+        id:7,
+        name: 'Wafsandwish',
+        tipo: 'dulce',
+        img:'',
+        ingredientes: ['Platano', 'Fresas', 'Moras Azules'],
+        recomendaciones: ['Cafe', 'Malteada', 'frape']
+    },
+    {
+        id:8,
+        name: 'WaflePizza',
+        tipo: 'dulce',
+        img:'',
+        ingredientes: ['Platano', 'Fresas', 'Moras Azules'],
+        recomendaciones: ['Cafe', 'Malteada', 'frape']
+    },
+]
