ee0e8d994f02e81065d23368243323f963686965
Luis Pina <luispia1290@gmail.com>
2024-08-07 18:03:53 -0600

    Se agrego la ruta para pagina de detalle

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Home } from "./pages";
+import { DetailTemplate } from "./templates/DetailTemplate";
 import { MainTemplate } from "./templates/MainTemplate";
 import { Route, Routes } from "react-router-dom";
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <Routes>
       <Route path="/witcherry/*" element={<MainTemplate />} />
+      <Route path="/witcherry/platillo/:id" element={<DetailTemplate />} />
       <Route path="/witcherry/home" element={<Home />} />
     </Routes>
   );
