c9f0e624112cbb38b21cb74eb86a09aa799ea295
Luis Pina <luispia1290@gmail.com>
2024-08-08 12:29:24 -0600

    Se  dividio el precio en funcion al diseño

--- src/components/ItemCarousel.tsx
+++ src/components/ItemCarousel.tsx
@@ -4,6 +4,7 @@ import { Title } from "./Title";
 import "./carousel.css";
 import { MouseEvent } from "react";
 import { useNavigate } from "react-router-dom";
+import { splicePrecio } from "../utils/splicePrecio";
 
 export interface PropsItems {
   idPlatillo: number;
@@ -20,11 +21,15 @@ export const ItemCarousel = ({
   img,
   precio,
 }: PropsItems) => {
+  let splitPrecio: string[] = [];
   const navigate = useNavigate();
   const handleClickDetail = (e: MouseEvent<HTMLElement>) => {
     e.preventDefault();
     navigate(`/witcherry/platillo/${idPlatillo}`);
   };
+
+  splitPrecio = splicePrecio(precio);
+
   return (
     <div onClick={handleClickDetail}>
       <Card
@@ -43,7 +48,10 @@ export const ItemCarousel = ({
         </div>
         <div className="card-footer">
           <div className="container-price">
-            <p>{`$${precio}`}</p>
+            <p>
+              <sup className="sub-text">$</sup>
+              {splitPrecio[0]}.<sup className="sub-text">{splitPrecio[1]}</sup>
+            </p>
           </div>
         </div>
       </Card>
