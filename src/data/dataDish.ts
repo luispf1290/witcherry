729b742348c2dcc98e58dcbbcbf5a2e0f7e62233
luis pina <luispia1290@gmail.com>
2024-10-19 17:47:43 -0600

    Se agrego ingrediente y precios

--- src/data/dataDish.ts
+++ src/data/dataDish.ts
@@ -6,7 +6,7 @@ export interface propsDish {
     tipo:string,
     img:string,
     precio:string
-    ingredientes: string[], 
+    ingredientes: string, 
     recomendaciones:string[]
 }
 
@@ -15,73 +15,82 @@ export const dishElements: propsDish[] = [
     {
         id:1,
         name: 'Caramelonions',
-        tipo: 'dulce',
+        tipo: 'Salado',
         img:dish,
-        precio:'45.99',
-        ingredientes: ['Platano', 'Fresas', 'Moras Azules'],
+        precio:'125.00',
+        ingredientes: 'Queso Manchego | cebolla caramelizada',
         recomendaciones: ['Cafe', 'Malteada', 'frape']
     },
     {
         id:2,
         name: 'Sangre de Bufalo',
-        tipo: 'dulce',
+        tipo: 'Salado',
         img:dish,
-        precio:'0.00',
-        ingredientes: ['Platano', 'Fresas', 'Moras Azules'],
+        precio:'195.00',
+        ingredientes: 'Fajitas de pollo con bufalo | Dip queso philadelphia con jalapeño | Moras Azules',
         recomendaciones: ['Cafe', 'Malteada', 'frape']
     },
     {
         id:3,
         name: 'Wafsandwish',
-        tipo: 'dulce',
+        tipo: 'Salado',
         img:dish,
-        precio:'0.00',
-        ingredientes: ['Platano', 'Fresas', 'Moras Azules'],
+        precio:'125.00',
+        ingredientes: 'Jamon | Queso Manchego',
         recomendaciones: ['Cafe', 'Malteada', 'frape']
     },
     {
         id:4,
-        name: 'WaflePizza',
-        tipo: 'dulce',
+        name: 'Americano',
+        tipo: 'Salado',
         img:dish,
-        precio:'0.00',
-        ingredientes: ['Platano', 'Fresas', 'Moras Azules'],
+        precio:'195.00',
+        ingredientes: 'Huevo | Tocino | Miel maple | Frutos rojos',
         recomendaciones: ['Cafe', 'Malteada', 'frape']
     },
     {
         id:5,
-        name: 'Chocobanana',
-        tipo: 'Dulce',
+        name: 'Hoguera',
+        tipo: 'Salado',
         img:dish,
-        precio:'0.00',
-        ingredientes: ['Platano', 'Fresas', 'Moras Azules', 'Miel'],
+        precio:'195.00',
+        ingredientes: 'Pollo Frito | Miel de Maple',
         recomendaciones: ['Cafe', 'Malteada', 'frape']
     },
     {
         id:6,
-        name: 'Bosque Rojo',
+        name: 'Chocobanana',
         tipo: 'Dulce',
         img:dish,
-        precio:'0.00',
-        ingredientes: ['Platano', 'Fresas', 'Moras Azules'],
+        precio:'170.00',
+        ingredientes: 'Nutela | Platano | Almendra | Conejito | Helado de Vainilla | Jarabe de Chocolate',
         recomendaciones: ['Cafe', 'Malteada', 'frape']
     },
     {
         id:7,
-        name: 'Tentacion de Eva',
+        name: 'Bosque Rojo',
         tipo: 'Dulce',
         img:dish,
-        precio:'0.00',
-        ingredientes: ['Platano', 'Fresas', 'Moras Azules'],
+        precio:'170.00',
+        ingredientes: 'Frutos rojos | Miel de maple | Helado de s´mores',
         recomendaciones: ['Cafe', 'Malteada', 'frape']
     },
     {
         id:8,
-        name: 'Brownie',
+        name: 'Tentacion de Eva',
         tipo: 'Dulce',
         img:dish,
-        precio:'0.00',
-        ingredientes: ['Platano', 'Fresas', 'Moras Azules'],
+        precio:'160.00',
+        ingredientes: 'Manzana Caramelizada | Granillo de nuez | Caramelo liquido | Helado de macadamia',
+        recomendaciones: ['Cafe', 'Malteada', 'frape']
+    },
+    {
+        id:9,
+        name: 'Sombra lunar',
+        tipo: 'Dulce',
+        img:dish,
+        precio:'195.00',
+        ingredientes: 'Wafle de chocolate | Brownie | Zarzamoras | jarabe de chocolate | Helado de vino y frutos rojos',
         recomendaciones: ['Cafe', 'Malteada', 'frape']
     },
     
